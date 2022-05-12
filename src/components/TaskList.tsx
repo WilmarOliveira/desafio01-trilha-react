503db4c1fb1abfc348d775e5b6a4bc21df8f5da3
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2022-05-11 22:16:58 -0300

    Challenge Completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { getByTitle } from '@testing-library/react';
 
 interface Task {
   id: number;
@@ -15,15 +16,29 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!!newTaskTitle) {
+      const newId = new Date().getTime()
+      setTasks([...tasks, {id: newId, title: newTaskTitle, isComplete: false}])
+      setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskMap = tasks.map(task => {
+      if (task.id === id) {
+        return {...task, isComplete: !task.isComplete}
+      }
+      return task
+    })
+    setTasks(taskMap)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const taskFilter = tasks.filter(task => {
+      return task.id !== id
+    })
+
+    setTasks(taskFilter)
   }
 
   return (
